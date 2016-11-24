876205353bb027ae51e15c31892f773ad983eac7
gajeshbhat <gajeshbht@gmail.com>
2016-11-25 01:23:15 +0530

    Comment Correction

--- EmailScraper.py
+++ EmailScraper.py
@@ -5,7 +5,7 @@ import pyperclip
 DataSet=pyperclip.paste() #Copies Data From Clipborad
 
 EmailRegxObj=re.compile(r'[a-zA-Z0-9_+.]+@[a-zA-Z0-9_+.]+') #Serching for Email Pattern
-EmailExtracts=EmailRegxObj.findall(DataSet) #Finds and Stores every eamil in the DataSet
+EmailExtracts=EmailRegxObj.findall(DataSet)  #Finds and Stores every eamil in the DataSet
 
 #Print Every Email line By line
 for email in EmailExtracts:
