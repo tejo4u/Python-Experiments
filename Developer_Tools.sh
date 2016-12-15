63ee2c88d7a1c8a913157f18beba28a97cee7b1e
Gajesh Bhat <gajeshbht@gmail.com>
2016-12-15 19:08:48 +0530

    DeveloperTools Script ubuntu
    Dev Tools Ubuntu Install Script

---
+++ Developer_Tools.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Author Gajesh Bhat
+# Shell Script to Install basic Dev Tools in Ubuntu or its Derivatives.
+
+#Clear the Screen before Starting the Install
+clear
+
+#Change the server to main server for update
