37e040bca0718460b319f19b6d6c3179060c03f4
Gajesh Bhat <gajeshbht@gmail.com>
2016-12-23 13:02:18 +0530

    Ubuntu_Script
    Ubuntu General Setup and Developer Tools Setup Script Commit

--- Developer_Tools.sh
+++ Developer_Tools.sh
@@ -5,3 +5,57 @@
 
 #Clear the Screen before Starting the Install
 clear
+
+#Updating the Software Repos and Uprading the System
+printf "Preparing the System for installation..."
+sleep 3
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+printf "Installing Developer Tools..."
+sleep 3
+
+#Build Essentials
+sudo apt-get -y install build-essential
+
+#Clang Support
+sudo apt-get -y install clang
+
+#Installing Git
+sudo apt-get -y install git
+
+#Configure git
+printf "Github Basic Account Info Setup...\n1.Enter Your Account User Name\n"
+read git_username
+git config --global user.name git_username
+printf "\n2.Enter Your Account Email Id\n"
+read git_useremail
+git config --global user.email git_useremail
+
+#Install C++14 Support for g++ and gcc
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get -y update
+sudo apt-get -y install g++-4.9
+# set g++4.9 as default compiler, the symlink in has to be updated:
+sudo ln -f -s /usr/bin/g++-4.9 /usr/bin/g++
+
+#Install Python tools
+sudo apt-get -y install python3
+sudo apt-get -y install python-pip
+
+#Install Java ,JDK and JRE
+sudo apt-get -y install default-jre
+sudo apt-get -y install default-jdk
+
+#Install Atom Text Editor
+sudo add-apt-repository ppa:webupd8team/atom -y
+sudo apt-get -y update
+sudo apt-get -y install atom
+
+#Install haskell
+sudo apt-get -y install haskell-platform
+
+#Additonal (Terminator terminal)
+sudo add-apt-repository ppa:nilarimogard/webupd8 -y && sudo apt-get -y update
+sudo apt-get install terminator
--- GeneralUbuntuSetup.sh
+++ GeneralUbuntuSetup.sh
@@ -1,21 +1,22 @@
+#!/bin/sh
+
+# Author Gajesh Bhat
+#Shell Scipt to Setup freshly Installed Ubuntu System.
+
 #Clear the Screen before Starting the Install
 clear
 
 #Change the server to main server for update
-printf "Changing the Mirror Server to Main Server..."
-sleep 3
+printf "Changing the Local Mirror Server to Main Server..."
 sudo sed -i 's|http://in.|http://|g' /etc/apt/sources.list
 
 #Enable Third Party Repos and Update the Repo List
-printf "Enabling Third-Party Repos and Proceding for Repository Update...."
-sleep 3
+printf "Enabling Third-Party Repos..."
 DISTRO=`cat /etc/*-release | grep DISTRIB_CODENAME | sed 's/.*=//g'`
 sudo sed -i.bak 's/\(# \)\(deb .*ubuntu '${DISTRO}' partner\)/\2/g' /etc/apt/sources.list
-sudo apt-get -y update
 
 #Removing Unity Lens and realted dash Plugins.
 printf "Removing Amazon Dash Plugins and Unity Dash lens for Amazon Services..."
-sleep 3
 sudo apt-get -y remove unity-lens-shopping
 gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
 gsettings set com.canonical.Unity.Lenses remote-content-search none
@@ -23,18 +24,20 @@ sudo rm /usr/share/applications/ubuntu-amazon-default.desktop
 sudo apt-get -y -f install
 
 
-printf "Proceding to Full System Update...."
-sleep 3
+printf "Updating Software Repos and Proceding to Full System Update...."
+sudo apt-get -y update
 sudo apt-get -y upgrade
 
 #Installing the Resticted Extras and VLC Media Player
 printf "Installing Restricted Extras.... (Audio and Video Plugins)"
-sleep 3
 sudo apt-get -y install ubuntu-restricted-extras
 printf "Installing VLC Player..."
-sleep 3
 sudo apt-get -y install vlc
 
 #Installing additional required Packages (Synaptic Package manager,Chromium,Unity Tweak Tool)
 printf "Installing Addtional Software..."
-printf "1.Synaptic Package manager\n2.Chromium Web Browser\n3.Unity Tweak Tool\n4."
+printf "1.Synaptic Package manager\n2.Chromium Web Browser\n3.Unity Tweak Tool\n4.GDebi Package Installer"
+sudo apt-get -y install chromium-browser #Chromium Web Browser
+sudo apt-get -y install synaptic # Synaptic Package manager
+sudo apt-get -y install unity-tweak-tool #Unity Tweak Tool
+sudo apt-get -y install gdebi #Gdebi Package Installer
